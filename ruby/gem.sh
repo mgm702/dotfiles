c8648054e0b8ff7411afc305434d5352b7c4c3f7
Matt Michnal <mattm3646@gmail.com>
2016-08-29 13:03:38 -0600

    Adding ruby configs to dotfile repo

---
+++ ruby/gem.sh
@@ -0,0 +1,14 @@
+ruby-install 2.2.3
+ruby-install 2.2.5
+ruby-install 2.3.0
+ruby-install mruby
+
+chruby ruby-2.2.5
+gem install bundler
+gem install pry
+gem install rspec
+gem install rake
+gem install lunchy
+gem install nokogiri
+gem install mechanize
+gem install rails -v 4.2.5
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing Ruby!"
+ln -sfv "$DOTFILES_DIR/ruby/irbrc" ~/.irbrc
+ln -sfv "$DOTFILES_DIR/ruby/railsrc" ~/.railsrc
+
+#Time to Source in the Ruby Gems
+. "$DOTFILES_DIR/ruby/gem.sh"
