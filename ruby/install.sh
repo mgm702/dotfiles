3bd0848d84a7fe908f4d112cec6892f07ffcd2c9
Matt Michnal <mmichnal@aimmedia.com>
2016-08-30 15:37:19 -0600

    Changing ruby version parsing on bash prompt

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,6 +3,7 @@
 echo "Installing Ruby!"
 ln -sfv "$DOTFILES_DIR/ruby/irbrc" ~/.irbrc
 ln -sfv "$DOTFILES_DIR/ruby/railsrc" ~/.railsrc
+ln -sfv "$DOTFILES_DIR/ruby/ruby-version" ~/.ruby-version
 
 #Time to Source in the Ruby Gems
 . "$DOTFILES_DIR/ruby/gem.sh"
