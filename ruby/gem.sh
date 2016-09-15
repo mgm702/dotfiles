72ed48b98166c849dbb960564fa54c7aa1096065
Matt Michnal <mattm3646@gmail.com>
2016-09-14 22:28:25 -0600

    Fixing syntax errors in gem,install,irbrc files

--- ruby/gem.sh
+++ ruby/gem.sh
@@ -3,11 +3,16 @@ ruby-install 2.2.5
 ruby-install 2.3.0
 ruby-install mruby
 
+# Either echo chruby source to bash profile
+# or source in bash_profile which is already loaded up from dotfiles
+
 chruby ruby-2.2.5
 gem install bundler
 gem install pry
 gem install rspec
 gem install rake
+gem install gem-path
+gem install looksee
 gem install lunchy
 gem install nokogiri
 gem install mechanize
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo "Installing Ruby!"
-ln -sfv "$DOTFILES_DIR/ruby/irbrc" ~/.irbrc
-ln -sfv "$DOTFILES_DIR/ruby/railsrc" ~/.railsrc
-ln -sfv "$DOTFILES_DIR/ruby/ruby-version" ~/.ruby-version
+ln -sfv "$DOTFILES_DIR/ruby/irbrc" $HOME/.irbrc
+ln -sfv "$DOTFILES_DIR/ruby/railsrc" $HOME/.railsrc
+ln -sfv "$DOTFILES_DIR/ruby/ruby-version" $HOME/.ruby-version
 
 #Time to Source in the Ruby Gems
 . "$DOTFILES_DIR/ruby/gem.sh"
