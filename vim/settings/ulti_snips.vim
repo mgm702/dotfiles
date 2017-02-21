27c228e2201f613393659af516f84c10085e5774
Matt Michnal <mattm3646@gmail.com>
2017-02-20 23:36:18 -0700

    Extra vim plugins are being added to dotfile repo

--- vim/settings/ulti_snips.vim
+++ vim/settings/ulti_snips.vim
@@ -1,6 +1,8 @@
 let g:UltiSnipsExpandTrigger="<tab>"
-let g:UltiSnipsJumpForwardTrigger="<tab>"
-let g:UltiSnipsJumpBackwardTrigger="<s-tab>"
+let g:UltiSnipsJumpForwardTrigger="<c-h>"
+let g:UltiSnipsJumpBackwardTrigger="<c-l>"
 
-" If you want :UltiSnipsEdit to split your window.
+let g:UltiSnipsListSnippets = '<c-s>'
+let g:UltiSnipsSnippetsDir="~/.vim/UltiSnips/"
 let g:UltiSnipsEditSplit="vertical"
+nnoremap <c-z> :UltiSnipsEdit<cr>
--- vim/setup.sh
+++ vim/setup.sh
@@ -2,12 +2,16 @@
 
 BUNDLE_DIR=~/.vim/bundle
 COLOR_DIR=~/.vim/colors
+SNIPPETS=~/.vim/UltiSnips
 
-# Install/update Vundle
-mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/VundleVim/Vundle.vim "$BUNDLE_DIR/vundle" || (cd "$BUNDLE_DIR/vundle" && git pull origin master))
+# Make the bundle directory for plugins
+mkdir -p "$BUNDLE_DIR"
 
 # Create vim directory on new system and setup everything else
 mkdir -p "$COLOR_DIR"
+
+# Creates directory for code snippets
+mkdir -p "$SNIPPETS"
 
 # Insert light/dark vim color scheme in system
 wget -t 45 https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim -O $COLOR_DIR/jellybeans.vim
@@ -16,13 +20,8 @@ wget -t 45 https://raw.githubusercontent.com/nelstrom/vim-mac-classic-theme/mast
 # Add symlinks for vim files
 ln -sfv "$DOTFILES_DIR/vim/vimrc" ~/.vimrc
 
-# Add symlinks for both the setting and vundle commands
+# Add symlinks for both the setting and plugin commands
 # Question is to symlink or no?
 
 # Install bundles
-vim +PluginInstall +qall
-
-# Compile YouCompleteMe
-cd "$BUNDLE_DIR/YouCompleteMe" && ./install.py
-
-cd -
+vim +PlugInstall +qall
